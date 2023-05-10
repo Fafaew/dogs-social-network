52cdbd835a2f89b9166b37c6a7b8f86d6d128370
fafaew <rafael.gomest@gmail.com>
2023-05-10 15:29:53 -0300

    Criar Login

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -4,7 +4,11 @@ const types = {
   email: {
     regex: /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i,
     message: 'Preencha um E-mail válido',
-  }
+  },
+  password: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    message:'A senha precisa ter 1 caractere maiúsculo, 1 caractere minúsculo e 1 dígito.com no mínimo 8 caracteres'
+  },
 }
 
 const useForm = (type) => {
