2d1b1cfa4cba120b9c05722a8b6d05debf8b60cc
fafaew <rafael.gomest@gmail.com>
2023-05-23 17:53:56 -0300

    Adicionados proptypes

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import FeedModal from './FeedModal';
 import FeedPhotos from './FeedPhotos';
+import PropTypes from 'prop-types';
 
 const Feed = ({ user }) => {
   const[modalPhoto, setModalPhoto] = React.useState(null);
@@ -9,7 +10,7 @@ const Feed = ({ user }) => {
 
   React.useEffect(() => {
     let wait = false
-    function infiniteScroll(e) {
+    function infiniteScroll() {
       if (infinite) {
         const scroll = window.scrollY;
         const height = document.body.offsetHeight - window.innerHeight;
@@ -45,6 +46,14 @@ const Feed = ({ user }) => {
       ))}      
     </div>
   )
+}
+
+Feed.defaultProps = {
+  user:0
+}
+
+Feed.propTypes = {
+  user: PropTypes.oneOfType([PropTypes.string.isRequired, PropTypes.number.isRequired]),
 }
 
 export default Feed
