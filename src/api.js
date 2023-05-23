47031470ba91b4365c9cb2325c30d4144d59531c
fafaew <rafael.gomest@gmail.com>
2023-05-23 19:52:34 -0300

    Perdeu a senha

--- src/api.js
+++ src/api.js
@@ -104,8 +104,34 @@ export function PHOTO_DELETE(id) {
     options: {
       method: 'DELETE',
       headers: {
-        Authorization: 'Bearer' + window.localStorage.getItem('token'),  
+        Authorization: 'Bearer' + window.localStorage.getItem('token'),
       },
     },
   };
+}
+
+export function PASSWORD_LOST(body) {
+  return {
+    url: API_URL + '/api/password/lost',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    }
+  }
+}
+
+export function PASSWORD_RESET(body) {
+  return {
+    url: API_URL + '/api/password/reset',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    }
+  }
 }
